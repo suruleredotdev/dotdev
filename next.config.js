288d34239a37577523150c3f4795041f1d3cd4c9
Korede Aderele <kaderele@gmail.com>
2022-10-22 16:31:09 -0700

    fix config typo

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withBundleAnalyzer({
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || ''
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || ''
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 
   staticPageGenerationTimeout: 300,
   images: {

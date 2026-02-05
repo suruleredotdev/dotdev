4325a5ec48a55ec9f8d705bde7ed3c716b3d3d2b
Korede Aderele <korede@surulere.dev>
2026-02-04 22:01:32 -0800

    BUILD: debug+error logs

--- lib/log.ts
+++ lib/log.ts
@@ -1,13 +1,17 @@
+const levels = [
+  "INFO", // debug INFO
+  "DEBUG", // debug DEBUG
+  "ERROR", // debug ERROR
+  // each subsequent level logs messages for previous levels
+];
+
 export function log(level: "INFO" | "DEBUG" | "ERROR", ...body: any[]) {
-  const levels = [
-    "INFO", // debug INFO
-    "DEBUG", // debug DEBUG
-    "ERROR", // debug ERROR
-    // each subsequent level logs messages for previous levels
-  ];
+  const logLevelSetting = (process.env.LOG_LEVEL || process.env.DEBUG)?.toUpperCase() || "INFO";
+
   const shouldLog = levels
     .slice(levels.indexOf(level))
-    .includes(process.env.DEBUG?.toUpperCase());
+    .includes(logLevelSetting);
+
   const logType: "warn" | "log" = level === "ERROR" ? "warn" : "log";
   if (shouldLog) console[logType](...body);
 }
